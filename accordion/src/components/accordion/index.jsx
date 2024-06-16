81d442638fc971913f2dc2a11a55c7e03f64801b
Erskine Gamaliel L. Duenas <erskine.duenas@gmail.com>
2024-06-16 14:24:06 +0800

    Accordion Component is completed
    -completed 06/16/2024
    -added some finishing touches like dynamic button and the button text being dynamic when being clicked

--- accordion/src/components/accordion/index.jsx
+++ accordion/src/components/accordion/index.jsx
@@ -1,3 +1,73 @@
-export default function Accordion () {
-    return <h1>Accordion</h1>
+//single selection
+
+import { useState } from "react";
+import data from "./data.js";
+import "./styles.css";
+
+//multiple selection
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId);
+    else copyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(copyMultiple);
+  }
+
+  console.log(selected, multiple);
+  return (
+    <div className="wrapper">
+      <button
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+        className={enableMultiSelection ? "button-enabled" : "button-disabled"}
+      >
+        {enableMultiSelection ? "Multi Selection" : "Single Selection"}
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+      <footer className="footer">Created by ED Duenas, 2024</footer>
+    </div>
+  );
 }
